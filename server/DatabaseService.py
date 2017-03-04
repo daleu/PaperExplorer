f37ade7aff9961e117223a4c0c3541394683058a
Eduard Maura <edu.maura.puig@gmail.com>
2017-03-04 14:24:04 +0100

    Add first query. Lack to parse it into python object

--- server/DatabaseService.py
+++ server/DatabaseService.py
@@ -1,13 +1,12 @@
-from neo4j.v1 import GraphDatabase, basic_auth, Node
+from neo4j.v1 import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo4j"))
 
 
 def find_paper_by_title(title):
     with driver.session() as session:
-        result = session.run('MATCH (paper:Paper {title:{title}}) RETURN paper.title', {"title": title})
-        nodes = []
+        result = session.run("MATCH(paper: Paper)-[:Cites]->(citation:Paper) WHERE paper.title =~ {title} RETURN properties(paper) as root, properties(citation) as child",
+                             {"title": title + '.*'})
         for item in result:
-            nodes.append(item)
+            print(item)
         return result
-
