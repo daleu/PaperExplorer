50071c6db585722406698e2d26fb4f00bc5fba3b
Eduard Maura <edu.maura.puig@gmail.com>
2017-03-04 19:24:03 +0100

    Fix database driver configuration

--- database/createGraph.py
+++ database/createGraph.py
@@ -4,28 +4,27 @@ import papers as pp
 
 NEO4J_USER = 'neo4j'
 NEO4J_PASSWORD = 'neo4j'
-URI = "bolt://localhost:7687"
+URI = "bolt://localhost"
+
 
 def create_nodes(papers):
     print("Creating NEO4j nodes")
-    driver = GraphDatabase.driver(URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
-    session = driver.session()
+    driver = GraphDatabase.driver(URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
 
     count = 0
     with driver.session() as session:
-        for key,value in papers.items():
-            result = session.run(
+        for key, value in papers.items():
+            session.run(
                 "CREATE (p:Paper {id: {id}, title: {title}, author: {author}, date: {date}})",
-            {"id": key, "title": value.title, "author": value.author, "date": value.date})
-            
+                {"id": key, "title": value.title, "author": value.author, "date": value.date})
+
             count += 1
             if count % 1000 == 0: print("Already processed {0} papers".format(count))
 
 
 def create_edges(papers):
     print("Creating NEO4j edges")
-    driver = GraphDatabase.driver(URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
-    session = driver.session()
+    driver = GraphDatabase.driver(URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
 
     count = 0
     with driver.session() as session:
@@ -33,9 +32,9 @@ def create_edges(papers):
         # Create an index on id to speed up the insertion of edges
         session.run("CREATE INDEX ON :Paper(id)")
 
-        for key,value in papers.items():
+        for key, value in papers.items():
             for citation in value.citations:
-                result = session.run(
+                session.run(
                     "MATCH (p1:Paper {id: {id1}}), (p2:Paper {id: {id2}}) \
                     CREATE (p1)-[:Cites]->(p2) CREATE (p2)-[:IsCitedBy]->(p1)",
                     {"id1": key, "id2": citation})
@@ -43,6 +42,8 @@ def create_edges(papers):
             count += 1
             if count % 1000 == 0: print("Already processed {0} papers".format(count))
 
+
 papers = pp.Papers()
 papers.load_papers()
+create_nodes(papers.papers)
 create_edges(papers.papers)
