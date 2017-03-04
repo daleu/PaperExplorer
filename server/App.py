9b0076753a23ed378bb81319fdbe3775742b663e
Eduard Maura <edu.maura.puig@gmail.com>
2017-03-05 00:36:54 +0100

    Add list feature

--- server/App.py
+++ server/App.py
@@ -36,5 +36,22 @@ def search():
                      'Access-Control-Allow-Headers': 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'}
         return r
 
+
+@app.route("/list")
+def list():
+    title = request.args.get('title')
+    if not title:
+        abort(400)
+    else:
+        papers = PaperService.list(title)
+        r = make_response(json.dumps(papers))
+        r.status_code = 200
+        r.headers = {'Content-Type': 'application/json',
+                     'Access-Control-Allow-Origin': '*',
+                     'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS',
+                     'Access-Control-Allow-Headers': 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'}
+        return r
+
+
 if __name__ == "__main__":
     app.run()
